f995ed4896e902597055611f66f7de034d55624b
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-09-17 17:22:51 +0200

    homomorphic mapped types preserve call and constructor signatures

---
+++ tests/cases/compiler/homomorphicMappedTypesPreserveCallConstructorSignatures.ts
@@ -0,0 +1,28 @@
+// @strict: true
+
+type YesWithRenaming<T> = { [P in keyof T as `${P & string}bla`]: { value: YesWithRenaming<T[P]> } };
+type YesWithoutRenaming<T> = { [P in keyof T]: { value: YesWithoutRenaming<T[P]> } };
+type NoBecauseNotHomomorphic<T> = { [P in keyof T & PropertyKey]: { value: NoBecauseNotHomomorphic<T[P]> } };
+
+interface Foo {
+  (): string;
+  (x: number): number;
+  prop: string;
+}
+
+interface Test {
+  a: string;
+  b: number;
+  (): boolean;
+  new(arg: unknown): { prop: unknown}
+  readonly nested: {
+    (): string;
+    c?: boolean;
+    new(arg: unknown): { prop: unknown}
+    readonly d: Foo
+  }
+}
+
+type T1 = YesWithRenaming<Test>
+type T2 = YesWithoutRenaming<Test>
+type T3 = NoBecauseNotHomomorphic<Test>
