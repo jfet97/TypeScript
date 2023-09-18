e7b62a160f0f5942831ec5385acd1a3d45cddf34
Andrea Simone Costa <andrysimo1997@gmail.com>
2023-09-18 14:24:24 +0200

    rename and update test

---
+++ tests/baselines/reference/revertedIntersection.js
@@ -0,0 +1,56 @@
+//// [tests/cases/compiler/revertedIntersection.ts] ////
+
+//// [revertedIntersection.ts]
+type StateConfig<TAction extends string> = {
+  entry?: TAction
+  states?: Record<string, StateConfig<TAction>>;
+};
+
+type StateSchema = {
+  states?: Record<string, StateSchema>;
+};
+
+declare function createMachine<
+  TConfig extends StateConfig<TAction>,
+  TAction extends string = TConfig["entry"] extends string ? TConfig["entry"] : string,
+>(config: { [K in keyof TConfig & keyof StateConfig<TAction>]: TConfig[K] }): [TAction, TConfig];
+
+const inferredParams1 = createMachine({
+  entry: "foo",
+  states: {
+    a: {
+      entry: "bar",
+    },
+  },
+  extra: 12,
+});
+
+const inferredParams2 = createMachine({
+  entry: "foo",
+  states: {
+    a: {
+      entry: "foo",
+    },
+  },
+  extra: 12,
+});
+
+//// [revertedIntersection.js]
+var inferredParams1 = createMachine({
+    entry: "foo",
+    states: {
+        a: {
+            entry: "bar",
+        },
+    },
+    extra: 12,
+});
+var inferredParams2 = createMachine({
+    entry: "foo",
+    states: {
+        a: {
+            entry: "foo",
+        },
+    },
+    extra: 12,
+});
