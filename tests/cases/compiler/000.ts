191e98c1e13c4970f0920e798fbb2a8140899da9
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-01-01 18:16:06 +0100

    tests

--- tests/cases/compiler/000.ts
+++ tests/cases/compiler/000.ts
@@ -1,3 +1,5 @@
+// @strict: true
+
 type NewReturnType<T extends (...args: never) => any> = T extends (
   ...args: never
 ) => infer R
@@ -8,8 +10,19 @@ type Foo<D extends Record<string, unknown>> = {
   [P in keyof Record<string, unknown>]: "whatever" extends D ? 1 : 0
 };
 
-type T0 = ReturnType<<D extends Record<string, unknown>>(t: D) => Foo<D>>
+// type T0 = ReturnType<<D extends Record<string, unknown>>(t: D) => Foo<D>>
   // ^?
 
 type T1 = NewReturnType<<D extends Record<string, unknown>>(t: D) => Foo<D>>
   // ^?
+
+// type T0 = ReturnType<<T>(t: T) => keyof T>
+
+// const fn = <T extends FromKeys<never>>(t: T): Foo<keyof T> => {
+//   return Object.fromEntries(
+//     Object.entries(t)
+//     .map(([k, v]) => [k, k === "whatever" ? 1 : 0] as const)
+//   ) as unknown as Foo<keyof T>;
+// }
+
+// type ExpectedReturnType = keyof unknown;
