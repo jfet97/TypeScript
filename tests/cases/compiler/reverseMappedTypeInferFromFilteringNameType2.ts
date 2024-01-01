0ab9e19afdf7376f5c89d648c4818b752cd1e1ba
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-01-01 02:16:13 +0100

    found bad example

--- tests/cases/compiler/reverseMappedTypeInferFromFilteringNameType2.ts
+++ tests/cases/compiler/reverseMappedTypeInferFromFilteringNameType2.ts
@@ -170,3 +170,21 @@ bombolo({ a: "a", b: "b", c: "c", enabled: true as const})
 
 // no excess property check because the parameter type turns out to be the empty object type {}
 bombolo({ a: "a", b: "b", c: "c"})
+
+
+// this is wrong: res should be { ok: "it's a string" } instead of {}
+// it's a bad idea using the source
+
+type Test<T> = {
+    [K in keyof T as T[K] extends string ? K : never ]: { a: T[K] }
+}
+
+declare function tst<T>(a: Test<T>): T
+
+const res = tst({
+  ok: { a: "it's a string" },
+  not_ok: { a: 123 },
+})
+
+   res
+// ^?
