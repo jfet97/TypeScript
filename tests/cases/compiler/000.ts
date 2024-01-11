f245c05c9a3d3e37654855696b4347c19d774e44
Andrea Simone Costa <andrysimo1997@gmail.com>
2024-01-11 22:45:37 +0100

    add test case

--- tests/cases/compiler/000.ts
+++ tests/cases/compiler/000.ts
@@ -13,13 +13,24 @@ type T2 = GetLabels<[a: 1, b: 2, c?: 3]> // TODO: I don't wanna '| undefined'
 type T3 = GetLabels<[a: 1, b: 2, ...c: number[]]>
 //    ^?
 
+
 type GetLabelsDeferred<T extends readonly any[]> = GetLabels<T>;
 
 type T4 = GetLabelsDeferred<[a: 1, b: 2, 3]> // TODO: doesn't work with generics yet
 //    ^?
 
+
 type T5 = GetLabels<number[]>
 //    ^?
 
 type T6 = GetLabels<{a: "smt"}[] | [...label1: string[], label2: 2] | [...string[], label3: 3]>
+//    ^?
+
+type T7 = GetLabels<number[]>
+//    ^?
+
+
+type GetLabelsWithTypeVariable<T> = GetLabels<[a: 1, b: T, 3]>;
+
+type T8 = GetLabelsWithTypeVariable<"unsure">
 //    ^?
